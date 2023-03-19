5d6f9dd4ae5a5f7302b58896805c67468c4c9630
Jeremy Shen <jeremyshen1987@gmail.com>
2023-03-18 21:38:54 -0600

    setup hamburger menu, intro page

---
+++ src/Flex.js
@@ -0,0 +1,44 @@
+import './Flex.css';
+
+
+function Flex(){
+
+    return(
+        <>
+            <nav className='navbar'>
+
+                <div className='companyName'>Company Name</div>
+
+                <a className='mobile_toggle_menu' onClick={toggle}>
+                    <span className='mobile_bar'></span>
+                    <span className='mobile_bar'></span>
+                    <span className='mobile_bar'></span>
+                </a>
+
+                <div className='nav_tabs'>
+                    <ul>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='about'>About</a></li>
+                        <li><a href='contact'>Contact</a></li>
+    
+                    </ul>
+                </div>
+    
+            </nav>
+    
+        </>
+    )
+
+    function toggle(){
+
+        const nav_buttons = document.getElementsByClassName('nav_tabs')[0]
+
+        nav_buttons.classList.toggle('active')
+
+    }
+
+}
+
+export default Flex;
+
+
---
+++ src/Portfolio.js
@@ -0,0 +1,37 @@
+import './Portfolio.css'
+
+
+function Portfolio(){
+
+    return(
+        <>
+
+        <div className='intro'>
+
+            <div className='intro_one'>
+                <span></span><h1>Hi, I'm Jeremy,</h1>
+                
+            </div>
+
+            <div className='intro_two'>
+                <span></span><h1>I want to be a web developer</h1>
+            </div>
+
+        </div>
+        <div>
+            <div className='project_container'>
+            
+            </div>
+            <div className='project_container'>
+            
+            </div>
+            <div className='project_container'>
+            
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default Portfolio;
